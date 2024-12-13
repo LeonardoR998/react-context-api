7cb2f5a5a319d3b395f70d39b3938ba4236164c8
LeonardoR998 <leonardoricci839@gmail.com>
2024-12-13 15:07:25 +0100

    add img in public and array in data

---
+++ src/data/posts.js
@@ -0,0 +1,39 @@
+const posts = [
+  {
+    id: 1,
+    title: "Ciambellone",
+    content: "Ciambellone ripieno.",
+    image: "/images/ciambellone.jpeg",
+    tags: ["soffice", "ripieno"],
+  },
+  {
+    id: 2,
+    title: "Cracker Barbabietola",
+    content: "Cracker alla barbabietola.",
+    image: "/images/cracker_barbabietola.jpeg",
+    tags: ["viola", "croccante"],
+  },
+  {
+    id: 3,
+    title: "Pane Fritto",
+    content: "Pane fritto e dolce.",
+    image: "/images/pane_fritto_dolce.jpeg",
+    tags: ["fritto", "dolce"],
+  },
+  {
+    id: 4,
+    title: "Pasta Barbabietola",
+    content: "Pasta alla barbabietola.",
+    image: "/images/pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola"],
+  },
+  {
+    id: 5,
+    title: "Torta Paesana",
+    content: "Torta di latte.",
+    image: "/images/torta_paesana.jpeg",
+    tags: ["latte", "torta"],
+  },
+];
+
+export default posts;
